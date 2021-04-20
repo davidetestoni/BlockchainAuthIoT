deae4cb4af205014553abf02a1ca246cb9168798
Davide Testoni <davide@testoni.net>
2021-04-21 00:48:29 +0200

    Fixed gas target decreasing too much

--- geth-clique-linux/startgeth.sh
+++ geth-clique-linux/startgeth.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rm -rf devChain/geth
 ./geth --datadir=devChain init genesis_clique.json
-./geth --nodiscover --datadir=devChain --http --http.port 8545 --http.addr 0.0.0.0 --http.corsdomain '*' --http.vhosts '*' --http.api 'eth,web3,personal,net,miner,admin,debug' --mine --allow-insecure-unlock --unlock 0x12890d2cce102216644c59daE5baed380d84830c --password "pass.txt" --verbosity 0 console
-# Add --miner.gaslimit 10000000 to limit the gas
+./geth --nodiscover --datadir=devChain --http --http.port 8545 --http.addr 0.0.0.0 --http.corsdomain '*' --http.vhosts '*' --http.api 'eth,web3,personal,net,miner,admin,debug' --mine --allow-insecure-unlock --unlock 0x12890d2cce102216644c59daE5baed380d84830c --password "pass.txt" --miner.gastarget 16234336 --miner.gaslimit 16234336 --verbosity 0 console
