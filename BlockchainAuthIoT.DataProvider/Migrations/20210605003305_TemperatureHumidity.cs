12a65961c79110ef5d1a35b0ab4d644a390dae0b
Davide Testoni <davide@testoni.net>
2021-06-05 02:34:32 +0200

    Added migration

---
+++ BlockchainAuthIoT.DataProvider/Migrations/20210605003305_TemperatureHumidity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlockchainAuthIoT.DataProvider.Migrations
+{
+    public partial class TemperatureHumidity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Humidity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Device = table.Column<string>(type: "longtext", nullable: true),
+                    Value = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Humidity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Temperature",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Device = table.Column<string>(type: "longtext", nullable: true),
+                    Value = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Temperature", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Humidity");
+
+            migrationBuilder.DropTable(
+                name: "Temperature");
+        }
+    }
+}
